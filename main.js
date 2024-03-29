06881ffec5295f15c3bd1cd6804ac68ca840795c
じぃ <161315295+Ji-113@users.noreply.github.com>
2024-03-29 22:11:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const addTasks = (task) => {
 
   // タスクに削除ボタンを付与
   const deleteButton = document.createElement('button');
-  deleteButton.innerHTML = '完了'.style.cursor: pointer;;
+  deleteButton.innerHTML = '完了';
   listItem.appendChild(deleteButton);
 
   // 削除ボタンをクリックし、イベントを発動（タスクが削除）
